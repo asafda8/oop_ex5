588f4286f7f7cbf575806ba6c422354f25459908
Oren Pinkas <pinkaso@cs.huji.ac.il>
2022-12-21 23:47:55 +0200

    added SunHalo class

--- PepseGameManager.java
+++ PepseGameManager.java
@@ -7,9 +7,19 @@ import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
+import danogl.gui.rendering.OvalRenderable;
 import danogl.util.Vector2;
 
+import java.awt.*;
+
 public class PepseGameManager extends GameManager {
+
+    private static final int SKY_LAYER = Layer.BACKGROUND;
+    private static final int SUN_LAYER = Layer.BACKGROUND + 1;
+    private static final int SUN_HALO_LAYER = SUN_LAYER + 1;
+    private static final int TERRAIN_LAYER = Layer.STATIC_OBJECTS;
+    private static final int NIGHT_LAYER = Layer.FOREGROUND;
+
     public PepseGameManager(String title, Vector2 windowDimensions) {
         super(title, windowDimensions);
     }
@@ -20,8 +30,11 @@ public class PepseGameManager extends GameManager {
 
         Vector2 windowDimensions = windowController.getWindowDimensions();
 
-        Sky.create(gameObjects(), windowDimensions, Layer.BACKGROUND);
-        Night.create(gameObjects(), Layer.FOREGROUND, windowDimensions, 30f);
+        Sky.create(gameObjects(), windowDimensions, SKY_LAYER);
+        Night.create(gameObjects(), NIGHT_LAYER, windowDimensions, 30f);
+//        GameObject sun = new GameObject(Vector2.ONES.multY(200), new Vector2(50, 50), new OvalRenderable(Color.WHITE));
+//        gameObjects().addGameObject(sun, SUN_LAYER);
+//        SunHalo.create(gameObjects(), SUN_HALO_LAYER, sun, new Color(255, 255, 0, 20));
     }
 
     public static void main(String[] args) {
---
+++ SunHalo.java
@@ -0,0 +1,37 @@
+package pepse.util;
+
+import danogl.GameObject;
+import danogl.collisions.GameObjectCollection;
+import danogl.components.Component;
+import danogl.components.CoordinateSpace;
+import danogl.gui.rendering.OvalRenderable;
+import danogl.util.Vector2;
+
+import java.awt.*;
+
+public class SunHalo {
+
+    public static GameObject create(GameObjectCollection gameObjects,
+                                    int layer, GameObject sun, Color color) {
+        GameObject sunHalo = new GameObject(
+                Vector2.ZERO,
+                sun.getDimensions().mult(2f),
+                new OvalRenderable(color)
+        );
+
+        sunHalo.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+
+        gameObjects.addGameObject(sunHalo, layer);
+
+        sunHalo.setTag("halo");
+
+        sunHalo.addComponent(new Component() {
+            @Override
+            public void update(float deltaTime) {
+                sunHalo.setCenter(sun.getCenter());
+            }
+        });
+
+        return sunHalo;
+    }
+}
