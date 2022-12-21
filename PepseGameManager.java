6a29983f27dfd60b451e61cd25aa5859d6fde7ed
Oren Pinkas <pinkaso@cs.huji.ac.il>
2022-12-21 21:30:28 +0200

    initial project start

---
+++ PepseGameManager.java
@@ -0,0 +1,13 @@
+package pepse.util;
+
+import danogl.GameManager;
+
+public class PepseGameManager extends GameManager {
+    public PepseGameManager() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        new PepseGameManager().run();
+    }
+}
