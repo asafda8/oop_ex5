a338182e786fa0b3418f78ef13bbec58cda7d933
asafda8 <85247344+asafda8@users.noreply.github.com>
2023-01-06 19:42:08 +0200

    add docs for sun and leaf

--- pepse/world/daynight/Sun.java
+++ pepse/world/daynight/Sun.java
@@ -11,15 +11,47 @@ import danogl.util.Vector2;
 import java.awt.*;
 
 public class Sun extends GameObject {
+    /**
+     * Starting x coordinate for the sun
+     */
     private static final int STARTING_X_COORDINATE = 50;
+    /**
+     * starting y coordinate for the sun
+     */
     private static final int STARTING_Y_COORDINATE = 50;
+    /**
+     * radius of the sun
+     */
     private static final int DIMENSION = 20;
+    /***
+     * Vector for initial position of the sun
+     */
     private static final Vector2 STARTING_POSITION_VECTOR =new Vector2(STARTING_X_COORDINATE, STARTING_Y_COORDINATE);
+    /**
+     * vector of dimensions of the sun
+     */
     private static final Vector2 DIMENSIONS =new Vector2(DIMENSION, DIMENSION);
+    /**
+     * First angle of movement at the start of the day
+     */
     private static final float FIRST_ANGLE = (float)Math.PI * 0.53f;
+    /**
+     * last angle of movement at the end of the day
+     */
     private static final float LAST_ANGLE = (float)Math.PI * 0.45f;
+    /***
+     * factor to multiply vector with for movement
+     */
     private static final float FACTOR_FOR_ANGLE = 0.45f;
 
+    /**
+     * creates a sun and returns its' instance
+     * @param gameObjects: objects of the game
+     * @param layer: layer to put sun in
+     * @param windowDimensions: dimensions of the window
+     * @param cycleLength: length of the cycle in which
+     * @return gameobject instance of the sun
+     */
     public static GameObject create(
             GameObjectCollection gameObjects,
             int layer,
@@ -36,10 +68,19 @@ public class Sun extends GameObject {
         return sun;
     }
 
+    /**
+     * returns the sun to its' initial position
+     */
     private void returnToInitial() {
         this.setCenter(STARTING_POSITION_VECTOR);
     }
 
+    /**
+     * private constructor for the sun
+     * @param topLeftCorner: top left corner vector of initial position
+     * @param dimensions: dimensions of the sun
+     * @param renderable: renderable for the sun
+     */
     private Sun(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable) {
         super(topLeftCorner, dimensions, renderable);
     }
--- pepse/world/trees/Leaf.java
+++ pepse/world/trees/Leaf.java
@@ -12,27 +12,92 @@ import java.awt.*;
 import java.util.Random;
 
 public class Leaf extends GameObject {
+    /**
+     * factor for getting the last value of dimensions
+     */
     private static final Float TRANSITION_FACTOR_FOR_DIMENSIONS = 1.05f;
+    /**
+     * difference between angles of the leaves
+     */
     private static final Float ANGLE_DIFFERENCE = 5f;
+    /**
+     * initial value for rotation transition
+     */
     private static final Float INITIAL_ROTATE_TRANSITION_VALUE = -5f;
+    /**
+     * final value for rotation transition
+     */
     private static final Float FINAL_ROTATE_TRANSITION_VALUE = 5f;
+    /**
+     * bound for waiting between rocking leaves
+     */
     private static final int BOUND_FOR_RANDOM_WAITING_TO_ADD = 5;
+    /**
+     * velocity of the fall of the leaves
+     */
     private final static Vector2 FALLING_VELOCITY = new Vector2(0, 10);
+    /**
+     * bound for waiting when leaves fall
+     */
     private final static int WAITING_BOUND = 100;
+    /**
+     * transition time for leaves between angles
+     */
     private static final int TRANSITION_TIME_FOR_ANGLE = 1;
+    /**
+     * transition time for leaves between dimensions
+     */
     private static final float TRANSITION_TIME_FOR_DIMENSIONS = .1f;
+    /**
+     * time for a leaf to fade out
+     */
     private final static int FADEOUT_TIME = 10;
+    /**
+     * mass value of the leaf
+     */
     private static final float NO_MASS = 0;
+    /**
+     * opaqueness value when creating the leaf
+     */
     private static final int FULL_OPAQUENESS = 1;
+    /**
+     * transition time when the leaves fall
+     */
     private static final int TRANSITION_TIME_TO_FALL = 10;
+    /**
+     * factor to multiply gaussian when calculating angle
+     */
     private static final Float FACTOR_FOR_ANGLE_CALCULATION = 1.5f;
+    /**
+     * maximum angle for the leaf
+     */
     private static final Float MAXIMUM_ANGLE = 5f;
+    /**
+     * generator for randomness
+     */
     private static final Random randomGenerator = new Random();
+    /**
+     * color of the leaf
+     */
     private static final Color LEAF_COLOR = new Color(50, 200, 30);
+    /**
+     * renderable to put on the leaf
+     */
     private static final RectangleRenderable LEAF_RENDERABLE = new RectangleRenderable(LEAF_COLOR);
+    /**
+     * dimensions of the leaf
+     */
     private static final Vector2 LEAF_DIMS = new Vector2(Block.SIZE, Block.SIZE);
+    /**
+     * initial position of the leaf
+     */
 
     private Vector2 initialPosition;
+
+    /**
+     * constructor for the leaf
+     * @param topLeftCorner: position of the leaf
+     */
 
     public Leaf(Vector2 topLeftCorner) {
         super(topLeftCorner, LEAF_DIMS, LEAF_RENDERABLE);
@@ -40,6 +105,10 @@ public class Leaf extends GameObject {
         this.initialPosition = topLeftCorner;
         startCycle();
     }
+
+    /**
+     * start cycle of the life of the leaf
+     */
     public void startCycle(){
         new ScheduledTask(this,
                 randomGenerator.nextFloat() +
@@ -52,6 +121,10 @@ public class Leaf extends GameObject {
                 false, this::setLeafToFall);
 
     }
+
+    /**
+     * set leaf to fall down
+     */
     private void setLeafToFall(){
         this.setVelocity(FALLING_VELOCITY);
         this.renderer().fadeOut(FADEOUT_TIME, this::endCycle);
@@ -66,6 +139,9 @@ public class Leaf extends GameObject {
                 null);
     }
 
+    /**
+     * set leaf to rock
+     */
     private void rockLeaf() {
         float angle = Math.max(Math.abs((float) randomGenerator.nextGaussian() *
                 FACTOR_FOR_ANGLE_CALCULATION), MAXIMUM_ANGLE);
@@ -90,10 +166,22 @@ public class Leaf extends GameObject {
                 null
         );
     }
+
+    /**
+     * end cycle of falling
+     */
     private void endCycle(){
         new ScheduledTask(this, Leaf.randomGenerator.nextInt(WAITING_BOUND),
                 false, this::startCycle);
     }
+
+    /**
+     * upon collision makes the leaf stay
+     * @param other The GameObject with which a collision occurred.
+     * @param collision Information regarding this collision.
+     *                  A reasonable elastic behavior can be achieved with:
+     *                  setVelocity(getVelocity().flipped(collision.getNormal()));
+     */
     @Override
     public void onCollisionEnter(GameObject other, Collision collision){
         setVelocity(Vector2.ZERO);
